d784eeabee8ca08d8ee1326dd4db43a1c95413f4
Rahul <royalchauhan300@gmail.com>
2023-12-06 21:28:33 +0530

    home

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { AiOutlineMessage } from "react-icons/ai";
 
 const home = () => {
   return (
     <>
-      <div className="flex h-[670px] py-14 bg-blue-100">
-        <div className="w-6/12 h-full pt-44 pl-10">
+      <div className="flex py-[21.5vh] bg-blue-100">
+        <div className="w-6/12 h-full pl-10 pt-5">
           <h1 className="text-5xl w-4/5">
             We <span className="text-red-600">Hindus</span> believe In the
             Vedas.
@@ -26,6 +27,15 @@ const home = () => {
             src="https://technowebstore.com/daksh-wordpress/wp-content/uploads/2023/04/s-images.png"
             className="h-[380px] absolute top-25 left-45"
           />
+        </div>
+      </div>
+
+      <div className="flex py-24 bg-red-600 text-white">
+        <div className="w-6/12 text-end">
+          <p className="text-[12vh] underline">The daksh platform</p>
+        </div>
+        <div className="w-6/12 text-8xl flex items-center justify-center">
+          <AiOutlineMessage className="hover:text-gray-400 outline outline-2 outline-offset-[20px] rounded-full" />
         </div>
       </div>
     </>
